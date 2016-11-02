4d73f8513cbfdfca0dd2c415431c402c566756c5
Spencer Manzon <samanzon@ucalgary.ca>
2016-11-02 15:37:00 -0600

    fitness centre screen

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
@@ -1,7 +1,9 @@
 package cpsc481.fall2016.uofcreccentret04;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    public void toFitnessCentre(View view) {
+
+        Intent toFitnessCentre = new Intent(view.getContext(),fitness_centre.class);
+        startActivityForResult(toFitnessCentre, 0);
     }
 }
