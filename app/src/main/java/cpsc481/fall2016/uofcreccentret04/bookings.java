0f9781210b4d4ce7d62750f7257148a18496e5db
paulinetelan <pauline.telan@yahoo.com>
2016-11-04 12:53:48 -0600

    changed title background color

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
@@ -1,5 +1,8 @@
 package cpsc481.fall2016.uofcreccentret04;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +13,9 @@ public class bookings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bookings);
         getSupportActionBar().setTitle("Book a Court");
-
+        // sets title background color
+        ActionBar bar = getSupportActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E30C00")));
+        bar.isHideOnContentScrollEnabled();
     }
 }
