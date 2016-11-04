46bd048923c5ae9acc9644089984da8ae6560e64
paulinetelan <pauline.telan@yahoo.com>
2016-11-04 14:00:49 -0600

    created button and listener for current bookings

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
@@ -5,8 +5,13 @@ import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class bookings extends AppCompatActivity {
+
+    // tracks if current bookings is shown
+    boolean showbookings = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +22,27 @@ public class bookings extends AppCompatActivity {
         ActionBar bar = getSupportActionBar();
         bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E30C00")));
         bar.isHideOnContentScrollEnabled();
+    }
+
+    // shows user's current booked rooms
+    public void toggleCurrentBookings(View view) {
+
+        ImageButton button = (ImageButton)findViewById(R.id.expandBookings);
+
+        if(showbookings == false){
+            // clear button image
+            button.setImageResource(android.R.color.transparent);
+            //change imagebutton image
+            button.setImageResource(R.drawable.collapse);
+
+            showbookings = true;
+
+        }else{
+            //change imagebutton image
+            button.setImageResource(android.R.color.transparent);
+            button.setImageResource(R.drawable.expand);
+
+            showbookings = false;
+        }
     }
 }
