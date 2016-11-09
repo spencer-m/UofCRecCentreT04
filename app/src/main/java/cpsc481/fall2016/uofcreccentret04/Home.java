d9d42dd8559b9e620cc61d0c125a2d790c9a39aa
Spencer Manzon <samanzon@ucalgary.ca>
2016-11-08 19:39:53 -0700

    beta version slidingdrawer

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
@@ -1,16 +1,56 @@
 package cpsc481.fall2016.uofcreccentret04;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SlidingDrawer;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    private SlidingDrawer drawer;
+    private Button handle, clickMe;
+    private TextView text1;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        context = this.getApplicationContext();
+
+        handle = (Button) findViewById(R.id.handle);
+        clickMe = (Button) findViewById(R.id.click);
+        drawer=(SlidingDrawer)findViewById(R.id.slidingDrawer);
+
+        drawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
+            @Override
+            public void onDrawerOpened() {
+                handle.setText("-");
+            }
+        });
+
+        drawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+
+            @Override
+            public void onDrawerClosed() {
+                handle.setText("+");
+            }
+        });
+
+        clickMe.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "The button is clicked", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/menu_view_sheet.java
@@ -0,0 +1,14 @@
+package cpsc481.fall2016.uofcreccentret04;
+
+import android.app.Dialog;
+import android.view.View;
+
+
+/**
+ * Created by Spencer on 11/8/2016.
+ */
+
+public class menu_view_sheet {
+
+
+}
