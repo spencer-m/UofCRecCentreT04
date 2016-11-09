d213f08ce7e6eab29264969c18c400955206e8d1
Spencer Manzon <samanzon@ucalgary.ca>
2016-11-08 23:36:43 -0700

    better sliding menu drawer

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/Home.java
@@ -4,55 +4,35 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.SlidingDrawer;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Home extends AppCompatActivity {
 
-    private SlidingDrawer drawer;
-    private Button handle, clickMe;
-    private TextView text1;
-    private Context context;
+    // Menu Dock Object
+    private SlidingUpPanelLayout menu_dock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        context = this.getApplicationContext();
+        menu_dock = (SlidingUpPanelLayout) findViewById(R.id.menu_dock);
 
-        handle = (Button) findViewById(R.id.handle);
-        clickMe = (Button) findViewById(R.id.click);
-        drawer=(SlidingDrawer)findViewById(R.id.slidingDrawer);
-
-        drawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
-            @Override
-            public void onDrawerOpened() {
-                handle.setText("-");
-            }
-        });
-
-        drawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
-
-            @Override
-            public void onDrawerClosed() {
-                handle.setText("+");
-            }
-        });
-
-        clickMe.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(context, "The button is clicked", Toast.LENGTH_LONG).show();
-            }
-        });
-
+        initMenuDock();
     }
-
 
     public void toFitnessCentre(View view) {
         Intent toFitnessCentre = new Intent(view.getContext(),fitness_centre.class);
@@ -64,5 +44,54 @@ public class Home extends AppCompatActivity {
         startActivityForResult(toBookings, 0);
     }
 
-    // its time
+    // Menu Dock Code
+
+    public void initMenuDock() {
+
+        ListView menuList = (ListView) findViewById(R.id.menu_list);
+        menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Home.this, "onItemClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<String> menuOptions = Arrays.asList(
+                "We",
+                "Are",
+                "Lit",
+                "Fam"
+        );
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> menuAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                menuOptions );
+
+        menuList.setAdapter(menuAdapter);
+
+        // TODO: figure out tapping on the grey area closes the menu dock
+
+        menu_dock.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu_dock.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (menu_dock != null &&
+                (menu_dock.getPanelState() == SlidingUpPanelLayout.PanelState.EXPANDED || menu_dock.getPanelState() == SlidingUpPanelLayout.PanelState.ANCHORED)) {
+            menu_dock.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
