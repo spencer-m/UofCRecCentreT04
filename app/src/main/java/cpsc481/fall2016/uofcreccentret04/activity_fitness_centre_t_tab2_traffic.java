ca67bdc3759b85de56fde8aeb218241bc5b0b65a
Nicole Ewert <nicole.ewert22@gmail.com>
2016-11-15 12:04:27 -0700

    Partway through tab implementation in fitness centre

---
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/activity_fitness_centre_t_tab2_traffic.java
@@ -0,0 +1,18 @@
+package cpsc481.fall2016.uofcreccentret04;
+
+/**
+ * Created by nicol on 11/15/2016.
+ */
+
+    import android.os.Bundle;
+    import android.support.v4.app.Fragment;
+    import android.view.LayoutInflater;
+    import android.view.View;
+    import android.view.ViewGroup;
+
+public class activity_fitness_centre_t_tab2_traffic extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fitness_centre_t_tab2_traffic, container, false);
+    }
+}
---
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/activity_fitness_centre_t_tab3_programs.java
@@ -0,0 +1,7 @@
+package cpsc481.fall2016.uofcreccentret04;
+
+/**
+ * Created by nicol on 11/15/2016.
+ */
+public class activity_fitness_centre_t_tab3_programs {
+}
