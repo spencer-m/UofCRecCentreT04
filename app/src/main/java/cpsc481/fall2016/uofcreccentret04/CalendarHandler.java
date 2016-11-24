53e2505f61dfb6cd2b66abe3c7a74174d3037356
paulinetelan <pauline.telan@yahoo.com>
2016-11-23 17:16:26 -0700

    FILTER WORKS!!! *we lit fam* - Spencer

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/CalendarHandler.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/CalendarHandler.java
@@ -206,7 +206,7 @@ public class CalendarHandler {
                     alertDialogBuilder.setNegativeButton("Cancel Booking", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                                 
+
                         }
                     });
                     Calendar st = event.getStartTime();
