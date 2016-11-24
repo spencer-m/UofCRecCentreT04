d70b997a314ea23ef7deff7a52bf95c248cf9438
paulinetelan <pauline.telan@yahoo.com>
2016-11-24 12:27:37 -0700

    FILTER WORKS!!! *we lit fam* - Spencer

--- app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
+++ app/src/main/java/cpsc481/fall2016/uofcreccentret04/bookings.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.ExpandableListAdapter;
@@ -27,13 +28,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
-public class bookings extends AppCompatActivity {
+public class Bookings extends AppCompatActivity {
 
     private WeekView wv;
 
     // Menu Dock Object
     MenuDock md;
     Calendar c = Calendar.getInstance();
+    CalendarHandler ch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +66,7 @@ public class bookings extends AppCompatActivity {
         bar.isHideOnContentScrollEnabled();
 
         // Create calendar
-        CalendarHandler ch = new CalendarHandler(this);
+        ch = new CalendarHandler(this);
 
         //ch.setWhatType(new String[] {"0", "1"});
 
@@ -72,11 +74,11 @@ public class bookings extends AppCompatActivity {
         wv = (WeekView) findViewById(R.id.weekView);
 
         // Set an action when any event is clicked.
-        wv.setOnEventClickListener(ch.simpleEventClickListener());
+        wv.setOnEventClickListener(ch.complexEventClickListener());
 
         // The week view has infinite scrolling horizontally. We have to provide the events of a
         // month every time the month changes on the week view.
-        wv.setMonthChangeListener(ch.simpleMonthChangeListener());
+        wv.setMonthChangeListener(ch.adaptiveMonthChangeListener());
 
         // Set long press listener for events.
         wv.setEventLongPressListener(ch.theEventLongPressListener());
@@ -88,6 +90,117 @@ public class bookings extends AppCompatActivity {
 
         // END CALENDAR
 
+        // handle Filter spinner
+        Spinner filter_spinner = (Spinner) findViewById(R.id.filter_booking_dropdown);
+        // set default value
+        filter_spinner.setSelection(2);
+        filter_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Spinner court_spinner = (Spinner) findViewById(R.id.court_booking_dropdown);
+                int court_type = court_spinner.getSelectedItemPosition();
+
+                // show All courts (mine, open racquet, open squash)
+                if(court_type == 0 && position == 2){
+                    ch.setWhatType(null);
+                }
+
+                // show all open courts
+                if(court_type == 0 && position == 0){
+                    ch.setWhatType(new String[] {"4", "5"});
+                }
+
+                // show open racquet courts
+                else if(court_type == 1 && position == 0){
+                    ch.setWhatType(new String[] {"5"});
+                }
+
+                // show open squash courts
+                else if(court_type == 2 && position == 0){
+                    ch.setWhatType(new String[] {"4"});
+                }
+
+                // show all my courts
+                else if(court_type == 0 && position == 1){
+                    ch.setWhatType(new String[] {"2", "3"});
+                }
+
+                // show my racquet courts
+                else if(court_type == 1 && position == 1){
+                    ch.setWhatType(new String[] {"3"});
+                }
+
+                // show my squash courts
+                else if(court_type == 2 && position == 1){
+                    ch.setWhatType(new String[] {"2"});
+                }
+
+                // refresh calendar
+                wv.notifyDatasetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        // handle Court filter
+        Spinner court_spinner = (Spinner) findViewById(R.id.court_booking_dropdown);
+        // set default value
+        court_spinner.setSelection(0);
+        court_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Spinner filter_spinner = (Spinner) findViewById(R.id.filter_booking_dropdown);
+                int filter_type = filter_spinner.getSelectedItemPosition();
+
+                // show All courts (mine, open racquet, open squash)
+                if(position == 0 && filter_type == 2){
+                    ch.setWhatType(null);
+                }
+
+                // show all open courts
+                if(position == 0 && filter_type == 0){
+                    ch.setWhatType(new String[] {"4", "5"});
+                }
+
+                // show open racquet courts
+                else if(position == 1 && filter_type == 0){
+                    ch.setWhatType(new String[] {"5"});
+                }
+
+                // show open squash courts
+                else if(position == 2 && filter_type == 0){
+                    ch.setWhatType(new String[] {"4"});
+                }
+
+                // show all my courts
+                else if(position == 0 && filter_type == 1){
+                    ch.setWhatType(new String[] {"2", "3"});
+                }
+
+                // show my racquet courts
+                else if(position == 1 && filter_type == 1){
+                    ch.setWhatType(new String[] {"3"});
+                }
+
+                // show my squash courts
+                else if(position == 2 && filter_type == 1){
+                    ch.setWhatType(new String[] {"2"});
+                }
+
+                // refresh calendar
+                wv.notifyDatasetChanged();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
 
     }
 
@@ -155,4 +268,5 @@ public class bookings extends AppCompatActivity {
         datePickerDialog.show();
 
     }
+
 }
